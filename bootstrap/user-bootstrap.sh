a7c41b7cb74460163e38aa9e929d531331a80760
Katsu Kawakami <kkawa1570@gmail.com>
2017-06-06 19:15:58 -0400

    Install Solarized and Powerline fonts

--- bootstrap/user-bootstrap.sh
+++ bootstrap/user-bootstrap.sh
@@ -1,11 +1,15 @@
 #!/bin/bash -x
 
+# Create a Repos directory for Git Repos
+mkdir Repos
+
 # Install all of the dotfiles
 cd ~
 git clone https://github.com/kkawakam/dotfiles
 cd dotfiles
 stow tmux
 stow vim
+stow zsh
 
 # Install Vundle for Vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
@@ -24,6 +28,22 @@ mv ~/.zshrc.pre-oh-my-zsh ~/.zshrc
 cd ~
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 source $HOME/.cargo/env
-rustup completions zsh > ~/.zfunc/_rustup
 rustup install stable
 rustup default stable
+mkdir -p ~/.zfunc
+rustup completions zsh > ~/.zfunc/_rustup
+
+# Clone Solarized for Gnome Terminal
+# User must installed this after they log in for the first time
+git clone https://github.com/Anthony25/gnome-terminal-colors-solarized.git  ~/Repos/gnome-terminal-colors-solarized
+cd ~/Repos/gnome-terminal-colors-solarized
+
+# Install missing powerline fonts
+mkdir ~/Downloads && cd ~/Downloads
+wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
+wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
+mkdir -p ~/.fonts
+mv PowerlineSymbols.otf ~/.fonts/
+mkdir -p ~/.config/fontconfig/conf.d #if directory doesn't exists
+fc-cache -vf ~/.fonts/
+mv 10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
